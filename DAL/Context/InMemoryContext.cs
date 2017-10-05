b2542e0ab2bfb6a2b2364baf9215874d8cef3d90
mashtoe <mashtoe@hotmail.com>
2017-10-05 11:31:17 +0200

    Fake Database

---
+++ DAL/Context/InMemoryContext.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Context
+{
+    public class InMemoryContext : DbContext
+    {
+        static DbContextOptions<InMemoryContext> options =
+            new DbContextOptionsBuilder<InMemoryContext>().UseInMemoryDatabase("TheDB").Options;
+
+
+        public InMemoryContext() : base(options)
+        {
+
+        }
+
+        public List<Product> products { get; set; }
+    }
+}
